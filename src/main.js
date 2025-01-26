8d7d19b12e93a2fdee2efeab8834b8feedf4d50e
Filippo-Crespi <crespifilippo123@gmail.com>
2025-01-26 22:07:40 +0100

    Aggiunta di PrimeVue e Axios, implementazione del sistema di toast per la gestione degli errori nel modulo

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 import "./assets/base.css";
-
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
+import Aura from "@primevue/themes/aura";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: "p",
+      darkModeSelector: "system",
+      cssLayer: false,
+    },
+  },
+});
+app.use(ToastService);
 
 app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,43 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import Toast from "primevue/toast";
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
+
+const code = ref("");
+
+function codeRegex(code) {
+  const regex = /$[a]/;
+  return regex.test(code);
+}
+function codeValid(code) {
+  if (!codeRegex(code)) {
+    toast.add({
+      severity: "error",
+      summary: "Errore",
+      detail: "Formato del codice non valido",
+    });
+    return;
+  }
+  // richiesta di ricerca del modulo
+  axios.get(`http://link.php/?code=${code}`).then((res) => {
+    if (res.status === 200) {
+      //redirect al modulo
+      return;
+    }
+    toast.add({
+      severity: "error",
+      summary: "Errore",
+      detail: "Errore durante la ricerca del modulo",
+    });
+  });
+}
+</script>
 
 <template>
+  <Toast />
   <div class="flex h-screen w-full">
     <div class="w-1/2 p-2 bg-blue-500">
       <div class="flex flex-col justify-center items-center h-full">
@@ -21,11 +58,13 @@
       <div class="flex flex-col justify-center items-center h-full">
         <h1 class="text-4xl font-bold">Compila un modulo</h1>
         <input
+          v-model="code"
           type="text"
           class="mt-4 px-4 py-2 w-1/3 text-center bg-white rounded-2xl"
           placeholder="Inserisci il codice"
         />
         <button
+          @click="codeValid(code)"
           class="mt-4 px-4 py-2 w-1/3 text-center bg-white rounded-2xl hover:bg-gray-200"
         >
           Entra
