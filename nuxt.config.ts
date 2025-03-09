d1bfffabde64dc9b46c199a647f4cf77813f2340
Filippo <crespifilippo123@gmail.com>
2025-03-09 14:58:04 +0100

    Nuxt full configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import Lara from "@primeuix/themes/lara";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/icon",
-    "@nuxtjs/google-fonts",
-    "@vueuse/nuxt",
-    "@nuxt/ui",
-    "@nuxtjs/tailwindcss",
-  ],
+  modules: ["@nuxt/icon", "@nuxtjs/google-fonts", "@vueuse/nuxt", "@primevue/nuxt-module"],
   css: ["~/assets/css/main.css"],
   googleFonts: {
     families: {
       Rubik: true,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  primevue: {
+    options: {
+      theme: {
+        preset: Lara,
+      },
     },
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
-<template>
-  <div></div>
-</template>
-
 <script lang="ts" setup></script>
 
-<style></style>
+<template>
+  <div class=""></div>
+</template>
+
+<style scoped></style>
